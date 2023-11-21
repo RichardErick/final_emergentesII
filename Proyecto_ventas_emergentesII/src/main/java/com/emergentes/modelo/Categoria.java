3fd153b0710b564ebc12d673ec6dda25288b85b4
RichardErick <63772886+RichardErick@users.noreply.github.com>
2023-11-21 00:53:50 -0400

    Add files via upload

---
+++ Proyecto_ventas_emergentesII/src/main/java/com/emergentes/modelo/Categoria.java
@@ -0,0 +1,10 @@
+
+package com.emergentes.modelo;
+
+
+public class Categoria {
+     private int id;
+    private String nombre_categoria;
+    private String descripcion;
+  
+}
---
+++ Proyecto_ventas_emergentesII/src/main/java/com/emergentes/modelo/Producto.java
@@ -0,0 +1,13 @@
+
+package com.emergentes.modelo;
+
+
+public class Producto {
+     private int id;
+    private String nombre;
+    private float precio;
+    private int stock;
+    private int monto_total;
+    private int precio_actual;
+    private int nombre_categoria;
+}
